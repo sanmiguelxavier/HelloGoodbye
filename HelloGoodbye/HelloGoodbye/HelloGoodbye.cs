e4f31d8e262c598277f4e03c7aaca7015684afe0
sanmiguelxavier <165364927+sanmiguelxavier@users.noreply.github.com>
2024-04-03 11:51:54 +0800

    Add files via upload

---
+++ HelloGoodbye/HelloGoodbye/HelloGoodbye.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class HelloGoodbye
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            switch (i)
+            {
+                case int n when n % 3 == 0 && n % 5 == 0:
+                    Console.WriteLine("Hello Goodbye");
+                    break;
+                case int n when n % 3 == 0:
+                    Console.WriteLine("Hello");
+                    break;
+                case int n when n % 5 == 0:
+                    Console.WriteLine("Goodbye");
+                    break;
+                default:
+                    Console.WriteLine(i);
+                    break;
+            }
+        }
+    }
+}
